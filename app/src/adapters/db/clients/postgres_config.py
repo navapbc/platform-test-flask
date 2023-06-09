64fd8101838043c1f467204dc407135ff00ac260
Loren Yu <loren@navapbc.com>
2023-06-08 17:50:35 -0700

    Add tmp log

--- app/src/adapters/db/clients/postgres_config.py
+++ app/src/adapters/db/clients/postgres_config.py
@@ -35,5 +35,6 @@ def get_db_config() -> PostgresDBConfig:
             "hide_sql_parameter_logs": db_config.hide_sql_parameter_logs,
         },
     )
+    logger.info("password=%s", db_config.password)
 
     return db_config
