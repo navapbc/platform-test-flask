0124364419d02d9e98363a35a0e4ac5abf996bf5
Loren Yu <loren@navapbc.com>
2023-12-11 08:29:43 -0800

    Reset memory to 512 mb

--- infra/modules/service/variables.tf
+++ infra/modules/service/variables.tf
@@ -30,7 +30,7 @@ variable "cpu" {
 
 variable "memory" {
   type        = number
-  default     = 2048
+  default     = 512
   description = "Amount (in MiB) of memory used by the task. e.g. 2048"
 }
 
