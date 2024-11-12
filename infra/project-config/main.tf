ffa945a1e371c628b9641ce31554cad8f77d2882
Tanner Doshier <git@doshitan.com>
2024-11-12 17:34:37 -0500

    Manually update base to version v0.12.4-45-gde5a358

--- infra/project-config/main.tf
+++ infra/project-config/main.tf
@@ -3,13 +3,15 @@ locals {
   # This will be used in names of AWS resources
   project_name = "platform-flask"
 
-  # Project owner
+  # Project owner (e.g. navapbc). Used for tagging infra resources.
   owner = "platform-admins"
 
   # URL of project source code repository
   code_repository_url = "https://github.com/navapbc/platform-test-flask"
 
-  # Default AWS region for project (e.g. us-east-1, us-east-2, us-west-1)
+  # Default AWS region for project (e.g. us-east-1, us-east-2, us-west-1).
+  # This is dependent on where your project is located (if regional)
+  # otherwise us-east-1 is a good default
   default_region = "us-east-1"
 
   github_actions_role_name = "${local.project_name}-github-actions"
